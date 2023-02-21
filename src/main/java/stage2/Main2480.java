8eab335ef76546b399c08d896ae27a3479110998
iee <horocolow@gmail.com>
2023-02-22 01:02:01 +0900

    2/22백준

---
+++ src/main/java/stage2/Main2480.java
@@ -0,0 +1,18 @@
+package stage2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d,e;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        if(a==b&& a==c){
+            System.out.println(a*1000+10000);
+        }
+        if(a == b || b==c || a==c){
+            if(a==b)
+        }
+}
--- src/main/java/stage2/Main2525.java
+++ src/main/java/stage2/Main2525.java
@@ -2,28 +2,25 @@ package stage2;
 
 import java.util.Scanner;
 
-public class Main2525 {
+public class Main2525{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int h,m,a,g;
+        int h,m,a,;
         h=sc.nextInt();
         m=sc.nextInt();
         a=sc.nextInt();
 
-        if(a+m>=60){
-            if(h=23){
-                h=0;
-            } else{
+        h+=a/60;
+        m+=a%60;
 
-
-
-                h++;
-            }
-          g = a+m-60;
-          m-=g;
-          System.out.println();
-        } else {
-            m+a;
+        if(m>60){
+            h+=1;
+            m-=60;
         }
+        if(h>=24){
+            h-=24;
+        }
+        System.out.println(h+""+m);
+
     }
 }
