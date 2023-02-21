e02d93a9729c4520c37963a3108ffcab5a8aae09
iee <horocolow@gmail.com>
2023-02-21 23:46:10 +0900

    2/21백준

---
+++ src/main/java/org/example/Main11382.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main11382 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.nextLong()+sc.nextLong()+sc.nextLong());
+
+    }
+
+}
