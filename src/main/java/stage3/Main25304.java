e7ea8757e311db58c0b59ca7578fb57cfa41e2c4
iee <horocolow@gmail.com>
2023-08-16 23:20:58 +0900

    8/16백준

---
+++ src/main/java/stage3/Main25304.java
@@ -0,0 +1,15 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class Main25304 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int count = sc.nextInt();
+
+
+
+    }
+}
